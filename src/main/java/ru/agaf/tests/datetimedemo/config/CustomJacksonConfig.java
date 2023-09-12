8e9b93b6f39658dbd684ac95b5a4cb3a8f1d7353
Naseko <Naseko@users.noreply.github.com>
2023-09-12 10:14:50 +0300

    +1 config example

--- src/main/java/ru/agaf/tests/datetimedemo/config/CustomJacksonConfig.java
+++ src/main/java/ru/agaf/tests/datetimedemo/config/CustomJacksonConfig.java
@@ -6,11 +6,19 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static ru.agaf.tests.datetimedemo.config.DateConfig.LOCAL_DATETIME_SERIALIZER;
+
 @Configuration
 public class CustomJacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> builder.serializationInclusion(JsonInclude.Include.NON_NULL)
                 .modules(new JavaTimeModule());
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.serializationInclusion(JsonInclude.Include.NON_NULL)
+                .serializers(LOCAL_DATETIME_SERIALIZER);
     }
 }
